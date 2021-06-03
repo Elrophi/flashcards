9c346579ba37089fa2c99972105692939cffc0eb
el <elrophilskwaila@gmail.com>
2021-06-03 15:49:20 +0300

    create templates

--- card/views.py
+++ card/views.py
@@ -1,5 +1,5 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 
@@ -9,5 +9,16 @@ def index(request):
 
     form = CardForm()
 
+    if request.method == 'POST':
+        form = CardForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
     context = {'cards':cards, 'form':form}
     return render(request, 'index.html', context)
+
+def updateCard(request, pk):
+    card = Card.object.get(id=pk)
+
+    return(request, 'update_card.html')
